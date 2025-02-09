02a1443d8d2ef4e8f9da9fd4a51a38a26d3104d3
A-CHERKAWI <abedelmouhsenc@gmail.com>
2025-02-09 18:50:40 -0500

    ClientManager

--- src/Driver/Main.java
+++ src/Driver/Main.java
@@ -4,7 +4,9 @@ package Driver;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+for (int i = 0; i<10000000; i++){
+    System.out.println(i);
+}
 
 
 
--- src/clients/ClientManager.java
+++ src/clients/ClientManager.java
@@ -4,33 +4,52 @@ public class ClientManager {
 
     private Client[] clientManager;
 
-    public ClientManager(int numClients) {
-        clientManager = new Client[numClients];
+    public ClientManager() {
+        clientManager = new Client[100000];
     }
 
 
     public void deleteClient(String email) {
-
-
         int index = 0;
-        Client[] temp = new Client[clientManager.length-1];
-        for(int i = 0; i < clientManager.length; i++) {
-            if (clientManager[i] != null && clientManager[i].getEmail().equals(email)) {
+        boolean emailFound = false;
+        for (int i = 0; i < clientManager.length; i++) {
+            if (clientManager[i].getEmail().equals(email)) {
                 index = i;
+                emailFound = true;
+                break;
+            }
+            else {emailFound = false;
             }
         }
 
-        int j = 0;
-        for(int i = 0; i < clientManager.length-1; i++) {
-            if(j!=index){
-                temp[i] = clientManager[j];
-            }
-            j++;
+        if (emailFound){
+
+        }
+        else {
+            System.out.println("Client with email \"" +email+  "\" does not exist");
         }
 
-        //clientManager = new ClientManager(temp);
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
 
-}
