de0a6c9c8e0fff45094179b352fec8abb7f651b3
A-CHERKAWI <abedelmouhsenc@gmail.com>
2025-02-09 20:14:52 -0500

    ClientManager

--- src/clients/ClientManager.java
+++ src/clients/ClientManager.java
@@ -1,11 +1,20 @@
 package clients;
 
-public class ClientManager {
+public class ClientManager extends Client {
 
     private Client[] clientManager;
 
     public ClientManager(int numClients) {
         clientManager = new Client[numClients];
+    }
+
+    public void addClient(String email) {
+       for (int i = 0; i < clientManager.length; i++) {
+           if (clientManager[i]!=null && clientManager[i].getEmail().equals(email)) {
+               System.out.println("Client already exists");
+           }
+       }
+
     }
 
 
