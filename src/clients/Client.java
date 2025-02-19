cbbf25b19f77ad8f21c991c5333b74493b4e85cf
Ahmad Khanafer <ahmad.khanafer.190@gmail.com>
2025-02-19 13:54:55 -0500

    commit

--- src/clients/Client.java
+++ src/clients/Client.java
@@ -9,13 +9,7 @@ public class Client {
     private String phoneNumber;
     private String address;
 
-    LeasingManager[] leaseList = new LeasingManager[3];
-
-
-
-
-
-
+    private LeasingManager[] leaseList = new LeasingManager[3];
 
     public Client(String name, String email, String phoneNumber, String address) {
         this.name = name;
@@ -30,51 +24,40 @@ public class Client {
         this.email = "";
         this.phoneNumber = "";
         this.address = "";
-
-
     }
 
-
-public Client(Client copy){
+    public Client(Client copy){
         this.name = copy.name;
         this.email = copy.email;
         this.phoneNumber = copy.phoneNumber;
         this.address = copy.address;
-}
+    }
 
+    public String getName() {
+            return name;
+    }
 
-
-public String getName() {
-        return name;
-}
-
-public String getEmail() {
-        return email;
-}
-public String getPhoneNumber() {
-        return phoneNumber;
-}
-public String getAddress() {
-        return address;
-}
-
-
-
-
-
-
-public void setName(String name) {
-        this.name = name;
-}
-public void setEmail(String email) {
-        this.email = email;
-}
-public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-}
-public void setAddress(String address) {
-        this.address = address;
-}
+    public String getEmail() {
+            return email;
+    }
+    public String getPhoneNumber() {
+            return phoneNumber;
+    }
+    public String getAddress() {
+            return address;
+    }
+    public void setName(String name) {
+            this.name = name;
+    }
+    public void setEmail(String email) {
+            this.email = email;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+    }
+    public void setAddress(String address) {
+            this.address = address;
+    }
 
     @Override
     public String toString() {
