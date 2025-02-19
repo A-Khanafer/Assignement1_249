a05ea31f0de5da7ec0556709851ac82c4db1554e
A-CHERKAWI <abedelmouhsenc@gmail.com>
2025-02-19 13:42:22 -0500

    clientManager perfected

--- src/Driver/Main.java
+++ src/Driver/Main.java
@@ -39,6 +39,44 @@ public class Main {
             System.out.println("\nAdditional operations:");
             System.out.println("12. Display the truck with the largest capacity");
             System.out.println("13. Create a copy of the electric trucks array");
+
+            System.out.println("\n0. Exit");
+
+            switch(choice){
+                case 1:
+                    break;
+                    case 2:
+                        break;
+                        case 3:
+                            break;
+                            case 4:
+                                break;
+                                case 5:
+                                    break;
+                                    case 6:
+                                        break;
+                                        case 7:
+                                            break;
+                                            case 8:
+                                                break;
+                                                case 9:
+                                                    break;
+                                                    case 10:
+                                                        break;
+                                                        case 11:
+                                                            break;
+                                                            case 12:
+                                                                break;
+                                                                case 13:
+                                                                    break;
+                                                                    case 0:
+                                                                        quit = true;
+                                                                        break;
+                                                                        default:
+                                                                            System.out.println("\nInvalid choice\n");
+                                                                            break;
+
+            }
         }
 
 
